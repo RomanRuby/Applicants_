270fe0e323b7f36005874817f8e062a9b7cc6ff9
RomanRuby <roman.nagibov@mail.ru>
2017-05-22 10:54:15 +0300

    init

--- src/main/java/cushing/Applicant.java
+++ src/main/java/cushing/Applicant.java
@@ -1,15 +1,19 @@
+package cushing;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class ApplicantsApplication {
+@EnableJpaRepositories
+public class Applicant {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApplicantsApplication.class, args);
+		SpringApplication.run(Applicant.class, args);
 	}
 
 
